e9866eede23ccca8ff324b6a7b5399ef8ab58197
Lafaiet <lafaietsilva@gmail.com>
2020-06-13 15:53:37 +0300

    Finishing forth class

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from products.views import HomeView, HomeView2, ProductListView, CreateProductView
+from products.views import HomeView, HomeView2, ProductListView, CreateProductView, UpdateProductView, DeleteProductView, DetailProductView, ContactView
 
 urlpatterns = [
+    path('contact/', ContactView.as_view(), name='contact'),
     path('home/', HomeView.as_view(), name='home'),
     path('home2/', HomeView2.as_view(), name='home2'),
     path('list/', ProductListView.as_view(), name='list'),
-    path('create/', CreateProductView.as_view(), name='create')
-
+    path('create/', CreateProductView.as_view(), name='create'),
+    path('detail/<int:pk>', DetailProductView.as_view(), name='detail'),
+    path('update/<int:pk>', UpdateProductView.as_view(), name='update'),
+    path('delete/<int:pk>', DeleteProductView.as_view(), name='delete')
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, CreateView
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView, DetailView, FormView
 from products.models import Product
+from products.forms import ContactForm
 from django.urls import reverse_lazy
 import random
 
@@ -42,4 +43,28 @@ class CreateProductView(CreateView):
     template_name = 'create.html'
     fields = '__all__'
     success_url = reverse_lazy('list')
+
+class DetailProductView(DetailView):
+    model = Product
+    template_name = 'detail.html'
+    context_object_name = 'product'
+
+class UpdateProductView(UpdateView):
+    model = Product
+    template_name = 'update.html'
+    context_object_name = 'product'
+    fields = '__all__'
+    success_url = reverse_lazy('list')
+
+class DeleteProductView(DeleteView):
+    model = Product
+    template_name = 'delete.html'
+    context_object_name = 'product'
+    success_url = reverse_lazy('list')
+
+class ContactView(FormView):
+    template_name = 'contact.html'
+    form_class = ContactForm
+    success_url = reverse_lazy('home')
+
 
