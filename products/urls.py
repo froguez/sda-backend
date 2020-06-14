d1bf94fd68a4d4a1e93bc64524bf17f259d13d00
Lafaiet <lafaietsilva@gmail.com>
2020-06-14 12:38:44 +0300

    middle of fifth class

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path, include
-from products.views import HomeView, HomeView2, ProductListView, CreateProductView, UpdateProductView, DeleteProductView, DetailProductView, ContactView
+from products.views import HomeView, HomeView2, ProductListView
+from products.views import CreateProductView, UpdateProductView
+from products.views import DeleteProductView, DetailProductView, ContactView
+from products.views import cookie_page_view, session_page_view, count_visited_view
 
 urlpatterns = [
+    path('count/', count_visited_view),
+    path('cookie/', cookie_page_view),
+    path('session/', session_page_view),
     path('contact/', ContactView.as_view(), name='contact'),
     path('home/', HomeView.as_view(), name='home'),
     path('home2/', HomeView2.as_view(), name='home2'),
--- products/views.py
+++ products/views.py
@@ -7,6 +7,44 @@ from django.urls import reverse_lazy
 import random
 
 
+
+def count_visited_view(request):
+    num_visited = 0
+    
+    response = HttpResponse('You have visited this page %d times'%(num_visited))
+    return response
+
+
+def session_page_view(request):
+    if not request.session.get('flavor'):
+        print("I don't know what cookie this user likes!")
+    else:
+        print("His favorite flavor is " + request.session.get('flavor'))
+
+    print(request.session)
+
+    request.session['flavor']['choco'] = 3
+    request.session['flavor']['vanilla'] = 10
+
+    request.session.modified = True
+
+    response = HttpResponse('Hello, world! Do you like cookies?')
+    return response
+
+
+def cookie_page_view(request):
+    if not request.COOKIES.get('flavor'):
+        print("I don't know what cookie this user likes!")
+    else:
+        print("His favorite flavor is " + request.COOKIES.get('flavor'))
+
+    print(request.COOKIES)
+    
+    response = HttpResponse('Hello, world! Do you like cookies?')
+    response.set_cookie('flavor', 'chocolate')
+    return response
+
+
 class HomeView(TemplateView):
     template_name = "home.html"
 
